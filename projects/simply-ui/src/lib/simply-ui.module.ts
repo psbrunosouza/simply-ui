37fde16691f6cf47f595a1b39c17df305c0cddd7
Bruno <psbrunosouza@gmail.com>
2023-03-09 12:04:25 -0300

    feat: first commit

--- projects/simply-ui/src/lib/simply-ui.module.ts
+++ projects/simply-ui/src/lib/simply-ui.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
-import { SimplyUiComponent } from './simply-ui.component';
-
-
 
 @NgModule({
   declarations: [
-    SimplyUiComponent
   ],
   imports: [
   ],
   exports: [
-    SimplyUiComponent
   ]
 })
 export class SimplyUiModule { }
--- projects/simply-ui/src/public-api.ts
+++ projects/simply-ui/src/public-api.ts
@@ -2,6 +2,4 @@
  * Public API Surface of simply-ui
  */
 
-export * from './lib/simply-ui.service';
-export * from './lib/simply-ui.component';
 export * from './lib/simply-ui.module';
