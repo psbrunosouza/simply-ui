d71f734f4b8f85785a5ac041f7ae8f8add71a9fb
Bruno <psbrunosouza@gmail.com>
2023-03-13 08:15:44 -0300

    fix: angular json storybook project

--- projects/tokens/src/colors.ts
+++ projects/tokens/src/colors.ts
@@ -1,18 +1,63 @@
 export const colors = {
+  //neutral
   white: '#FFF',
   black: '#000',
 
+  // gray
   gray100: '#E1E1E6',
   gray200: '#A9A9B2',
-  gray400: '#7C7C8A',
-  gray500: '#505059',
-  gray600: '#323238',
-  gray700: '#29292E',
-  gray800: '#202024',
-  gray900: '#121214',
+  gray300: '#7C7C8A',
+  gray400: '#505059',
+  gray500: '#323238',
+  gray600: '#29292E',
+  gray700: '#202024',
+  gray800: '#121214',
+  gray900: '#080809',
 
-  ignite300: '#00B37E',
-  ignite500: '#00875F',
-  ignite700: '#015F43',
-  ignite900: '#00291D',
+  //main
+  main300: '#fffffe',
+  main500: '#f1f1f0',
+  main700: '#d9d9d8',
+  main900: '#b9b9b8',
+
+  // highlight
+  highlight300: '#c0e1fa',
+  highlight500: '#3da9fc',
+  highlight700: '#3089cc',
+  highlight900: '#215b88',
+
+  // details
+  details300: '#f8ccd4',
+  details500: '#ef4565',
+  details700: '#d23c58',
+  details900: '#942a3e',
+
+  // paragraph
+  paragraph300: '#8393a6',
+  paragraph500: '#5f6c7b',
+  paragraph900: '#46505b',
+
+  // header
+  header300: '#265d83',
+  header500: '#094067',
+  header900: '#062b46',
+
+  //success
+  success300: '#b5fab9',
+  success500: '#49ef53',
+  success700: '#3dc746',
+  success900: '#2b8c31',
+
+  //danger
+  danger300: '#fab5b5',
+  danger500: '#ef4949',
+  danger700: '#c73d3d',
+  danger900: '#8c2b2b',
+
+  //warning
+  warning300: '#f8ecb3',
+  warning500: '#efd349',
+  warning700: '#d0b73f',
+  warning900: '#9f8c31',
+
 }
